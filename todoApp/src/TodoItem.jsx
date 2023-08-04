ea4a69c416c96bbdae37e004a0043a72515eeadf
aftabkhan07 <khanaftaba1@gmail.com>
2023-08-04 17:37:07 +0530

    Todo App

---
+++ todoApp/src/TodoItem.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function TodoItem({ completed, id, title, toggleTodo, deleteTodo }) {
+  return (
+    <li>
+      <label>
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={(e) => toggleTodo(id, e.target.checked)}
+        />
+        {title}
+      </label>
+      <button
+        className="btn btn-danger"
+        onClick={() => deleteTodo(id)}
+      >
+        Delete
+      </button>
+    </li>
+  );
+}
+
+export default TodoItem;
---
+++ todoApp/src/TodoList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+
+function TodoList({ todos, toggleTodo, deleteTodo }) {
+  return (
+    <ul className="list">
+      {todos.length === 0 && "No Todos"}
+      {todos.map((todo) => {
+        return (
+          <TodoItem
+          // {...todo} We can do this instead of the below 3 lines it will just spread it 
+            id={todo.id}
+            completed={todo.completed}
+            title={todo.title}
+            key={todo.id}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        );
+      })}
+    </ul>
+  );
+}
+
+export default TodoList;
